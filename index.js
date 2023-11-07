b43bd2f4343d82965e7a457569ab3aef3866522a
alimran-dev <alimran5air@gmail.com>
2023-11-08 03:45:25 +0600

    books get api added

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
     res.send('shelfbud server is running...');
 })
@@ -26,10 +31,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const database = client.db("ShelfBud");
+    const bookCollection = database.collection("books");
+
+    app.get('/books', async (req, res) => {
+      const result = await bookCollection.find().toArray();
+      res.send(result);
+    })
+    app.post('/books', async (req, res) => {
+      const book = req.body;
+      console.log(book);
+      const result = await bookCollection.insertOne(book);
+      res.send(result);
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
