5312beddc1e941b59e8bacbbbc3d056549eaa8fe
alimran-dev <alimran5air@gmail.com>
2023-11-10 05:04:21 +0600

    orders post api added

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send('shelfbud server is running...');
-})
-
-
+app.get("/", (req, res) => {
+  res.send("shelfbud server is running...");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tklaef2.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,35 +21,49 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
+const database = client.db("ShelfBud");
+const bookCollection = database.collection("books");
+const orderCollection = database.collection("orders");
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-    const database = client.db("ShelfBud");
-    const bookCollection = database.collection("books");
-
-    app.get('/books', async (req, res) => {
+    // books API
+    app.get("/books", async (req, res) => {
       const result = await bookCollection.find().toArray();
       res.send(result);
-    })
-    app.get('/services/:id', async (req, res) => {
+    });
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const result = await bookCollection.find({ _id: new ObjectId(id) }).toArray();
+      const result = await bookCollection
+        .find({ _id: new ObjectId(id) })
+        .toArray();
       res.send(result);
-    })
-    app.post('/books', async (req, res) => {
+    });
+
+    app.post("/books", async (req, res) => {
       const book = req.body;
       console.log(book);
       const result = await bookCollection.insertOne(book);
       res.send(result);
-    })
+    });
+
+    // Order API
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      console.log(order);
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -61,7 +71,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-    console.log(`shelfbud server is running on: ${port}`);
-})
+  console.log(`shelfbud server is running on: ${port}`);
+});
